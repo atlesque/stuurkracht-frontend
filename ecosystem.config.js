1d9b4fd5bae42a9db4c582b69cef472ba4b34623
Alexander Van Maele <alexander@atlesque.com>
2020-12-05 18:16:18 +0100

    Update PM2 process name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "StuurkrachtFrontend",
+      name: "stuurkracht-frontend",
       exec_mode: "cluster",
       instances: "max",
       script: "./node_modules/nuxt/bin/nuxt.js",
