7854d42c6988cd2daed287adb62a935418b369a1
Alexander Van Maele <alexander@atlesque.com>
2020-12-05 20:11:52 +0100

    Use single PM2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,11 @@ module.exports = {
   apps: [
     {
       name: "stuurkracht-frontend",
-      exec_mode: "cluster",
-      instances: "max",
       script: "./node_modules/nuxt/bin/nuxt.js",
+      // Cluster mode gives a port conflict. Use single instance unless performance suffers
+      // exec_mode: "cluster",
+      // instances: "max",
+      instances: 1,
       args: "start",
       watch: true,
       exp_backoff_restart_delay: 100,
